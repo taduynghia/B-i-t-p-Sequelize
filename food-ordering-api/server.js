e951231c6d27b48e03934db1b33b2c400488ed12
nghiatarenovacloud <nghia.ta@renovacloud.com>
2024-07-20 16:15:06 +0700

    push code

--- food-ordering-api/server.js
+++ food-ordering-api/server.js
@@ -1,6 +1,8 @@
 import app from './app.js';
 import sequelize from './config/database.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync({ force: true }).then(() => {
